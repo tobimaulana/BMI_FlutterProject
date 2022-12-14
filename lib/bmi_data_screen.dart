4335d78a82b5749a0b426e10c94541e9753dfbb5
Tobi Maulana <tobimaulana.tm@gmail.com>
2022-12-14 09:10:34 +0700

    Configure a Stateful & Stateless Widget - first screen

---
+++ lib/bmi_data_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class BmiDataScreen extends StatefulWidget {
+  const BmiDataScreen({Key? key}) : super(key: key);
+
+  @override
+  State<BmiDataScreen> createState() => _BmiDataScreenState();
+}
+
+class _BmiDataScreenState extends State<BmiDataScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+
+    );
+  }
+}
