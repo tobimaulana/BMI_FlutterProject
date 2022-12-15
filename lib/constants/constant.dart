ff9f7b6d059960ec531f43f68a024d822669b1e9
Tobi Maulana <tobimaulana.tm@gmail.com>
2022-12-15 09:51:09 +0700

    Add constant.dart to saving a boilerplate code (ex: color, textstyle)

---
+++ lib/constants/constant.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+Color primaryBGColor = const Color(0xff12153B);
+Color primaryBGColorAccent = const Color(0xff1E285D);
+TextStyle genderTextStyle = const TextStyle(
+                              fontSize: 17,
+                              color: Colors.white,
+                            );
