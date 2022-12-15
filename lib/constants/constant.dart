9797eb6d4cda2c6aebe87f690494e7ac5211be2b
Tobi Maulana <tobimaulana.tm@gmail.com>
2022-12-15 16:14:52 +0700

    Add numberTextStyle variant in constant.dart

--- lib/constants/constant.dart
+++ lib/constants/constant.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 Color primaryBGColor = const Color(0xff12153B);
 Color primaryBGColorAccent = const Color(0xff1E285D);
+Color secondaryColorForBtnInCard= const Color(0xff7175A9);
 TextStyle genderTextStyle = const TextStyle(
                               fontSize: 17,
                               color: Colors.white,
@@ -9,4 +10,10 @@ TextStyle genderTextStyle = const TextStyle(
 TextStyle labelTextStyle = const TextStyle(
   fontSize: 17,
   color: Colors.white,
+);
+
+TextStyle numberTextStyle = const TextStyle(
+    fontSize: 50,
+    fontWeight: FontWeight.w900,
+    color: Colors.white
 );
