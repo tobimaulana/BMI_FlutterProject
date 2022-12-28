f72603c2b2ac34025bc1b8dad120e7774f5c4297
Tobi Maulana <tobimaulana.tm@gmail.com>
2022-12-29 06:19:42 +0700

    Create a function to determine the category of BMI calculation results (IF ELSEIF)

--- lib/views/bmi_result_screen.dart
+++ lib/views/bmi_result_screen.dart
@@ -1,3 +1,4 @@
+import 'package:body_mass_index/constants/constant.dart';
 import 'package:body_mass_index/views/bmi_data_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -8,6 +9,47 @@ class BmiResultScreen extends StatelessWidget {
   // Variabel bmi -> berupa hasil hitung
   // Kemudian tambahkan variabel bmi ke parameter disamping key
   final double bmi;
+
+  // Buat fungsi u/ menentukan kategori hasil perhitungan BMI
+  // Menerima masukan berupa double
+  determineBmiCategory(double bmiValue) {
+      // Nilai akan disimpan sementara sbg variabel lokal
+      String category = "";
+
+      if (bmiValue < 16.0) {
+        category = underweightSevere;
+      }
+
+      else if (bmiValue >= 16.0 && bmiValue < 17) {
+        category = underweightModerate;
+      }
+
+      else if (bmiValue >= 17.0 && bmiValue < 18.5) {
+        category = underweightMild;
+      }
+
+      else if (bmiValue >= 18.5 && bmiValue < 25.0) {
+        category = normal;
+      }
+
+      else if (bmiValue >= 25.0 && bmiValue < 30.0) {
+        category = overweight;
+      }
+
+      else if (bmiValue >= 30.0 && bmiValue < 35.0) {
+        category = obese1;
+      }
+
+      else if (bmiValue >= 35.0 && bmiValue < 40.0) {
+        category = obese2;
+      }
+
+      else if (bmiValue >= 40.0) {
+        category = obese3;
+      }
+
+      return category;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -38,8 +80,8 @@ class BmiResultScreen extends StatelessWidget {
                 // MainAxisAlignment.spaceEvenly
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  const Text("Normal",
-                  style: TextStyle(
+                  Text(determineBmiCategory(bmi),
+                  style: const TextStyle(
                       fontSize: 20,
                       fontWeight: FontWeight.bold,
                       color: Colors.white
